2a0558f960e3046546b6eace18e0eded6ba5343b
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-04-18 10:19:00 +0200

    Now you can run linux commands in expsh

--- expsh.c
+++ expsh.c
@@ -3,53 +3,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include <readline/readline.h>
 
 #define MAX_ARG_LEN 256
-#define INIT_CAP      8
+#define INIT_CAP    8
 
 typedef struct {
-    const char** args;
+    char** args;
     size_t count;
     size_t cap;
-} Args_t;
+} args_t;
 
-int parse_args(const char** args, size_t count);
+int parse_args(char** args, size_t count);
 
-void args_alloc(Args_t* a, size_t wanted_cap);
+void args_alloc(args_t* a, size_t wanted_cap);
 
-void args_append(Args_t* a, const char* arg);
-void args_append_many(Args_t* a, const char** args, size_t count);
+void args_append(args_t* a, const char* arg);
+void args_append_many(args_t* a, const char** args, size_t count);
 #define args_append_more(a, ...) {args_vappend((a), __VA_ARGS__, NULL)}
-void args_vappend(Args_t* a, ...);
+void args_vappend(args_t* a, ...);
 
-void args_print(Args_t args);
+void args_print(args_t args);
 
-Args_t get_args(char* line);
+args_t get_args(char* line);
 char* sdup(const char* str);
 
 int main(int argc, char* argv[]) {
     int quit = 0;
 
-    Args_t args = {0};
-    size_t count;
+    args_t args = {0};
     const char* prompt = "$ ";
     while (!quit && !feof(stdin)) {
         char* buff = readline(prompt);
         args = get_args(buff);
-        args_print(args);
+/*         args_print(args); */
         quit = parse_args(args.args, args.count);
     }
     return 0;
 }
 
-int parse_args(const char** args, size_t count) {
-
+int parse_args(char** args, size_t count) {
+    int res;
+    if (!strcmp(args[0], "exit") || !strcmp(args[0], "quit")) {
+        return 1;
+    }
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("Cannot fork a process");
+    } else if (pid == 0) {
+        if (execvp(args[0], (char* const*)args) == -1) {
+            perror("Cannot execute a process");
+        }
+    }
+    waitpid(-1, &res, 0);
+    return WEXITSTATUS(res);
 }
 
-Args_t get_args(char* line) {
-    Args_t args = {0};
+args_t get_args(char* line) {
+    args_t args = {0};
     const char* delim = " \n\r\t\b\a";
     char* tmp = sdup(line);
     char* token = strtok(tmp, delim);
@@ -57,17 +71,18 @@ Args_t get_args(char* line) {
         args_append(&args, token);
         token = strtok(NULL, delim);
     }
+    args.args[args.count++] = NULL;
     free(tmp);
 
     return args;
 }
 
-void args_append(Args_t* a, const char* arg) {
+void args_append(args_t* a, const char* arg) {
     args_alloc(a, a->count+1);
     memcpy(a->args[a->count++], arg, strlen(arg) + 1);
 }
 
-void args_alloc(Args_t* a, size_t wanted_cap) {
+void args_alloc(args_t* a, size_t wanted_cap) {
     if (wanted_cap >= a->cap) {
         if (a->cap == 0) a->cap = INIT_CAP;
 
@@ -80,7 +95,7 @@ void args_alloc(Args_t* a, size_t wanted_cap) {
     }
 }
 
-void args_append_many(Args_t* a, const char** args, size_t count) {
+void args_append_many(args_t* a, const char** args, size_t count) {
     args_alloc(a, a->count+count);
     for (size_t i = 0; i < count; ++i) {
         memcpy(a->args[a->count+i], args[i], strlen(args[i]) + 1);
@@ -88,18 +103,18 @@ void args_append_many(Args_t* a, const char** args, size_t count) {
     a->count += count;
 }
 
-void args_vappend(Args_t* a, ...) {
+void args_vappend(args_t* a, ...) {
     va_list ap;
     va_start(ap, a);
     char* arg; 
-    while ((arg = va_arg(ap, char*))) {
+    while ((arg = va_arg(ap, char*)) != NULL) {
         args_alloc(a, a->count+1);
         memcpy(a->args[a->count++], arg, strlen(arg) + 1);
     }
     va_end(ap);
 }
 
-void args_print(Args_t args) {
+void args_print(args_t args) {
     printf("[");
     for (int i = 0; i < args.count; ++i) {
         if (i == args.count-1) printf("%s", args.args[i]);
